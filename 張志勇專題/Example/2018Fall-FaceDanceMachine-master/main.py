9a19dd199474d6503bffcbd63a301fead3445dc3
Jorson-Lee <83799211+Jorson-Lee@users.noreply.github.com>
2022-02-12 13:16:03 +0800

    自Google Drive搬運

---
+++ 張志勇專題/Example/2018Fall-FaceDanceMachine-master/main.py
@@ -0,0 +1,27 @@
+import os
+import pygame as pg
+from FaceDanceMachine import FaceDanceMachine
+from Similarity import Similarity
+
+
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d"%(30,0)
+
+def main():
+    similarity = Similarity()
+
+    pg.init() 
+
+    display_width = 740
+    display_height = 480
+
+    clock = pg.time.Clock()
+    gameDisplay = pg.display.set_mode([display_width,display_height])
+    pg.display.set_caption('Face Dance Machine') 
+    
+    faceDanceMachine = FaceDanceMachine(gameDisplay, similarity)
+    faceDanceMachine.run()
+    pg.quit()
+
+if  __name__ == '__main__':
+    main()
